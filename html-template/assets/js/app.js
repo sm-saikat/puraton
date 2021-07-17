0df79d025efa8d4a5294753cefab3e8171f794a0
sm-saikat <mail.smsaikat@gmail.com>
2021-07-17 09:52:03 +0600

    Favorite Button  functionality added

--- html-template/assets/js/app.js
+++ html-template/assets/js/app.js
@@ -1,1 +1,25 @@
+const favBtnUnchecked = document.querySelectorAll('.fav-btn-unchecked');
+const favBtnChecked = document.querySelectorAll('.fav-btn-checked');
+const favBtns = document.querySelectorAll('.fav-btn');
 
+
+
+
+
+// Fav Btn Functionality
+
+[...favBtns].forEach(btn => {
+
+    btn.onclick = e => {
+
+        let id = e.target.dataset.id;
+        let selectedBtns;
+
+        e.target.classList.toggle('hide');
+
+        selectedBtns = (e.target.classList.contains('fav-btn-unchecked')) ? favBtnChecked : favBtnUnchecked;
+
+        let clickedBtn = [...selectedBtns].find( btn => btn.dataset.id === id);
+        clickedBtn.classList.toggle('hide');
+    }
+})
